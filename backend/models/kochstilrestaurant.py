cf0e3fdca191f3199028ad27cca2c6b42828fabc
Erik Siehr <erik@MacBook-Air-von-Erik.local>
2025-11-19 10:23:02 +0100

    Models created

---
+++ backend/models/kochstilrestaurant.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Integer, Column, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class KochstilRestaurant(Base):
+    __tablename__ = 'kochstilrestaurant'
+
+    stilid = Column(Integer, ForeignKey('kochstil.stilid'), primary_key=True)
+    restaurantid = Column(Integer, ForeignKey('restaurant.restaurantid'), primary_key=True)
+
+    kochstil = relationship("kochstil", back_populates="restaurant")
+    restaurant = relationship("restaurant", back_populates="kochstil")
+
+def to_dict(self):
+    return {
+        "stilid": self.stilid,
+        "restaurantid": self.restaurantid
+    }
